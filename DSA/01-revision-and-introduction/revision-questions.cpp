f23ba7230bdc331a74ed9f65811fa348fef4c2f2
Alexander Dimitrov <adimitrov23@gmail.com>
2020-08-29 14:10:41 +0300

    Add files via upload

---
+++ DSA/01-revision-and-introduction/revision-questions.cpp
@@ -0,0 +1,17 @@
+
+		/* Въпроси за преговор */
+
+// Има ли проблеми и какво ще изведе следната функция?
+void func()
+{
+	char*	text	= "FMI";
+	char*	buffer	= new char[60];
+	while ( *text )
+	{
+		*buffer = *text;
+		++buffer;
+		++text;
+	}
+	
+	std::cout << buffer;
+}
